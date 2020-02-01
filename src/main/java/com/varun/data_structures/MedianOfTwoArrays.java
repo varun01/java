148c6686fdf4c4f9868701d4be0100a1374ff878
Varun kumar Kancharla <vkancharla@pkrmsezdc.com>
2020-02-01 18:38:50 +0530

    ignoring target

--- src/main/java/com/varun/data_structures/MedianOfTwoArrays.java
+++ src/main/java/com/varun/data_structures/MedianOfTwoArrays.java
@@ -2,6 +2,11 @@ package com.varun.data_structures;
 
 import java.util.Arrays;
 
+/**
+ * Median for merging Two arrays of Equal length.
+ * @author Varun
+ *
+ */
 public class MedianOfTwoArrays {
 
 	public static void main(String[] args) {
@@ -11,14 +16,19 @@ public class MedianOfTwoArrays {
 		int[] result = obj.mergeArrays(arr1, arr2);
 		Arrays.sort(result);
 		int length = result.length;
-		System.out.println(5/2);
 		if(length %2 ==0) {
-			System.out.println("Even median is: "+(result[(length/2)-1] + result[(length/2)])/2);
+			System.out.println(" Median is: "+(result[(length/2)-1] + result[(length/2)])/2);
 		} else {
-			System.out.println("Odd median is: "+ result[(length/2)]);
+			System.out.println(" Median is: "+ result[(length/2)]);
 		}
 	}
 	
+	/**
+	 * Merging Two arrays of equal length
+	 * @param arr1
+	 * @param arr2
+	 * @return
+	 */
 	int[] mergeArrays(int[] arr1, int[] arr2) {
 		int[] result = new int[2*arr1.length];
 		for(int i = 0; i < arr1.length ; i++ ) {
